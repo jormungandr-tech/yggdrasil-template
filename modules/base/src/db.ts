5b63bddd224cbe05b30a2fdba098e5d3ef4aa086
jormungandr-tech <jor@plr.moe>
2024-07-30 16:22:30 +0800

    remove vercel postgre support

--- modules/base/src/db.ts
+++ modules/base/src/db.ts
@@ -3,9 +3,8 @@ import Redis from 'ioredis';
 
 import { drizzle as pgJsDrizzle } from 'drizzle-orm/postgres-js';
 import { drizzle as neonDrizzle } from 'drizzle-orm/neon-http';
-import { drizzle as vercelDrizzle } from 'drizzle-orm/vercel-postgres';
 
-export type Postgres = ReturnType<typeof pgJsDrizzle> | ReturnType<typeof neonDrizzle> | ReturnType<typeof vercelDrizzle>;
+export type Postgres = ReturnType<typeof pgJsDrizzle> | ReturnType<typeof neonDrizzle>;
 
 export type DatabaseAccessor = IO<Postgres>
 
